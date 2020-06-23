0baf087ec0bea1173855b7db10f6ce1ea78a0806
OolongQian <qiansucheng2017@outlook.com>
2020-06-23 12:38:38 +0800

    update look and move

--- src/ur_perception/script/look_and_move.py
+++ src/ur_perception/script/look_and_move.py
@@ -11,12 +11,11 @@ import scipy.io as scio
 
 bridge = CvBridge()
 
-
 """
 Robot execution GETTING_STARTED: 
 roscore 
 roslaunch ur_perception camera_setup.launch 
-roslaunch ur_modern_driver ur5_bringup.launch  // drive robot hardware and publish physical information. 
+roslaunch ur_modern_driver ur5_bringup.launch robot_ip:=192.168.1.102  // drive robot hardware and publish physical information. 
 roslaunch ur5_moveit_config demo.launch  // initialize moveit essential service and infrastructure. 
 
 EXECUTE this script.
@@ -45,8 +44,37 @@ Dataset class information
 18: '040_large_marker',
 19: '051_large_clamp', 
 20: '052_extra_large_clamp', 
-21: '061_foam_brick')
+21: '061_foam_brick'
 """
+# TODO : refactor this utility function in ur_perception package.
+id2name = {1: '002_master_chef_can',
+           2: '003_cracker_box',
+           3: '004_sugar_box',
+           4: '005_tomato_soup_can',
+           5: '006_mustard_bottle',
+           6: '007_tuna_fish_can',
+           7: '008_pudding_box',
+           8: '009_gelatin_box',
+           9: '010_potted_meat_can',
+           10: '011_banana',
+           11: '019_pitcher_base',
+           12: '021_bleach_cleanser',
+           13: '024_bowl',
+           14: '025_mug',
+           15: '035_power_drill',
+           16: '036_wood_block',
+           17: '037_scissors',
+           18: '040_large_marker',
+           19: '051_large_clamp',
+           20: '052_extra_large_clamp',
+           21: '061_foam_brick'}
+
+
+def clsId2clsName(cls_id):
+    """convert mmdetection predicted output class id to class name.
+    all classes can be inspected under ur_perception/models.
+    cls_id starts from 1, and 1 is background class, therefore 002_master_chef_can has class id 2."""
+    return id2name[cls_id - 1]
 
 
 class Eyes:
@@ -81,7 +109,7 @@ class Eyes:
             # self.myImshow(cv_rgb, 1)
             # self.myImshow(cv_depth, 2)
             # plt.show()
-            self.detectAndEstimate(cv_rgb, cv_depth)
+            return self.detectAndEstimate(cv_rgb, cv_depth)
         except CvBridgeError:
             rospy.logerr("cv bridge error")
 
@@ -146,7 +174,9 @@ class Eyes:
 
 
 from moveit_python import MoveGroupInterface, PlanningSceneInterface
-from geometry_msgs.msg import Pose, Point, Quaternion
+import geometry_msgs.msg
+from tf import TransformListener
+
 
 class Terminator:
     def __init__(self, ur_perception_ros_path, perception_repos_path):
@@ -161,22 +191,54 @@ class Terminator:
         "MoveGroupInterface is used for goal specification and control."
         self.g = MoveGroupInterface(group="manipulator", frame="base_link")
 
-    def addCube(self):
-        input("press any key to add a cube in the scene to see whether this works.")
-        self.p.addCube("my_cube", 0.1, 1, 0, 0.5)
+        self.tf_listener = TransformListener()
 
-    def removeCube(self):
-        input("press any key to remove the cube in the scene to see whether this works.")
-        self.p.removeCollisionObject("my_cube")
+    def buildScene(self):
+        obj_poses = self.eyes.blink()
 
-    def addMesh(self):
-        input("press any key to add a cube in the scene to see whether this works.")
-        mesh_name = "../models/024_bowl/textured.obj"
-        self.p.addMesh(name="bowl", pose=Pose(Point(x=0.1, y=0.1, z=0.1), Quaternion()), filename=mesh_name)
+        for i in range(obj_poses.shape[0]):
+            obj_pose = obj_poses[i]
+            cls_id, trans, rot = obj_pose[0], obj_pose[1:4], obj_pose[4:]
+            cls_name = clsId2clsName(cls_id)
+            instance_name = "{}_{}".format(cls_name, i)
 
-    def removeMesh(self):
-        input("press any key to remove the cube in the scene to see whether this works.")
-        self.p.removeCollisionObject("bowl")
+            # t = self.tf_listener.getLatestCommonTime("base_link", "camera_link")
+            camera_pose = geometry_msgs.msg.PoseStamped()
+            camera_pose.header.frame_id = "camera_link"
+            camera_pose.pose.position.x, camera_pose.pose.position.y, camera_pose.pose.position.z = trans[1], trans[0], \
+                                                                                                    trans[2]
+            camera_pose.pose.orientation.x, camera_pose.pose.orientation.y, camera_pose.pose.orientation.z, camera_pose.pose.orientation.w = rot
+            base_pose = self.tf_listener.transformPose("base_link", camera_pose)
+
+            self.addMesh(clsId2clsName(cls_id), instance_name, base_pose.pose.position, base_pose.pose.orientation)
+
+    def addMesh(self, cls_name, instance_name, point, quaternion):
+        print "begin add object {} of class {}".format(instance_name, cls_name)
+        mesh_name = "../models/{}/textured.obj".format(cls_name)
+        self.p.addMesh(name=instance_name, pose=geometry_msgs.msg.Pose(point, quaternion), filename=mesh_name)
+
+
+def eyeTest(ur_perception_ros_path, perception_repos_path):
+    eyes = Eyes(ur_perception_ros_path, perception_repos_path)
+    eyes.blink()
+
+
+def buildSceneTest(ur_perception_ros_path, perception_repos_path):
+    terminator = Terminator(ur_perception_ros_path, perception_repos_path)
+    terminator.buildScene()
+
+
+def tfTest():
+    tf_listener = TransformListener()
+    camera_pose = geometry_msgs.msg.PoseStamped()
+    camera_pose.header.frame_id = "/camera_link"
+    camera_pose.pose.position.x, camera_pose.pose.position.y, camera_pose.pose.position.z = 0, 0, 0
+    camera_pose.pose.orientation.x, camera_pose.pose.orientation.y, camera_pose.pose.orientation.z = 0, 0, 0
+    camera_pose.pose.orientation.w = 1.0
+    import time
+    time.sleep(2)
+    base_pose = tf_listener.transformPose("/base_link", camera_pose)
+    a = 1
 
 
 if __name__ == "__main__":
@@ -185,10 +247,13 @@ if __name__ == "__main__":
     ur_perception_ros_path = "/home/vinjohn/sucheng/Robotics/src/ur_perception"
     perception_repos_path = "/home/vinjohn/sucheng/Robotics/perception"
 
-    # eyes = Eyes(ur_perception_ros_path, perception_repos_path)
+    # eyeTest(ur_perception_ros_path, perception_repos_path)
+    buildSceneTest(ur_perception_ros_path, perception_repos_path)
+    # tfTest()
 
-    terminator = Terminator(ur_perception_ros_path, perception_repos_path)
+    # terminator = Terminator(ur_perception_ros_path, perception_repos_path)
+    # terminator.eyes.blink()
     # terminator.addCube()
     # terminator.removeCube()
-    terminator.addMesh()
-    terminator.removeMesh()
+    # terminator.addMesh()
+    # terminator.removeMesh()
